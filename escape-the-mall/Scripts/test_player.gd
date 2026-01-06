88140b01be1b2dafa1821182328a1767c442b06b
Marcellusjpg <tommieluc@gmail.com>
2026-01-06 13:12:35 +0100

    Wasd added

--- escape-the-mall/Scripts/test_player.gd
+++ escape-the-mall/Scripts/test_player.gd
@@ -5,7 +5,9 @@ extends CharacterBody3D
 const SPEED = 5.0
 const JUMP_VELOCITY = 4.5
 
-var senseHorizontal = 0.5
+#sensetivity aanpassen 
+var sensHorizontal = 0.5
+var sensVertical = 0.5
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
@@ -17,15 +19,15 @@ func _ready():
 
 func _input(event):
 	if event is InputEventMouseMotion:
-		rotate_y(deg_to_rad(-event.relative.x * senseHorizontal))
-
+		rotate_y(deg_to_rad(-event.relative.x * sensHorizontal))
+		camera_mount.rotate_x(deg_to_rad(-event.relative.y * sensVertical))
 	# Handle jump.
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var input_dir := Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
+	var input_dir := Input.get_vector("MoveLeft", "MoveRight", "MoveForward", "MoveBackwards")
 	var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	if direction:
 		velocity.x = direction.x * SPEED
