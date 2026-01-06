f1bcbe55c6707ed798a1ae9238c1558adc2bcd03
BjornKorsten <586133@student.fontys.nl>
2026-01-06 14:09:37 +0100

    succes

--- escape-the-mall/Scripts/test_player.gd
+++ escape-the-mall/Scripts/test_player.gd
@@ -1,37 +1,44 @@
 extends CharacterBody3D
+
 @onready var camera_mount: Node3D = $cameraMount
 
+const SPEED := 5.0
+const JUMP_VELOCITY := 4.5
+var sense_horizontal := 0.5
 
-const SPEED = 5.0
-const JUMP_VELOCITY = 4.5
+func _ready() -> void:
+	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
+	print("READY")
+	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
 
-var senseHorizontal = 0.5
+
+func _input(event: InputEvent) -> void:
+	if event is InputEventMouseMotion:
+		rotate_y(deg_to_rad(-event.relative.x * sense_horizontal))
+	if event is InputEventKey and event.pressed:
+		print("KEY:", event.keycode)
 
 func _physics_process(delta: float) -> void:
-	# Add the gravity.
+
+	# Gravity
 	if not is_on_floor():
 		velocity += get_gravity() * delta
 
-func _ready():
-	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
-
-func _input(event):
-	if event is InputEventMouseMotion:
-		rotate_y(deg_to_rad(-event.relative.x * senseHorizontal))
-
-	# Handle jump.
+	# Jump
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
+	# Move (ui_left/right/up/down bestaan standaard)
 	var input_dir := Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
+
 	var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
-	if direction:
+
+	if direction != Vector3.ZERO:
 		velocity.x = direction.x * SPEED
 		velocity.z = direction.z * SPEED
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-		velocity.z = move_toward(velocity.z, 0, SPEED)
+		velocity.x = move_toward(velocity.x, 0.0, SPEED)
+		velocity.z = move_toward(velocity.z, 0.0, SPEED)
 
 	move_and_slide()
+	
